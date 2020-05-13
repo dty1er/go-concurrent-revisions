5bc9dcc1c8aeb4ce68bb2f12fe51373360bca491
Hidetatsu Yaginuma <deetyler@protonmail.com>
2020-05-13 17:02:21 +0900

    fork revision

--- revision.go
+++ revision.go
@@ -1,5 +1,20 @@
 package conrev
 
 type Revision struct {
-	Root *Segment
+	Root       *Segment
+	CurrentRev *Segment
+
+	done chan struct{}
 }
+
+func NewRevision(root, current *Segment) *Revision {
+	return &Revision{Root: root, CurrentRev: current}
+}
+
+func (r *Revision) Fork(a Action) {
+	nr := NewRevision(r.Root, NewSegmentWithParent(r.CurrentRev))
+	r.CurrentRev.Release()
+	r.CurrentRev = NewSegmentWithParent(r.CurrentRev)
+}
+
+// run action, wait it
