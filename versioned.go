bb8888bc4405c8bd6aa6569c0d125b9e996424b8
Hidetatsu Yaginuma <deetyler@protonmail.com>
2020-05-26 11:23:51 +0900

    implement versioned.Get

--- versioned.go
+++ versioned.go
@@ -10,6 +10,17 @@ type Versioned struct {
 	versions *sync.Map
 }
 
+func (ve *Versioned) get(r *Revision) interface{} {
+	s := r.current
+	for {
+		v, ok := ve.versions.Load(s.version)
+		if ok {
+			return v
+		}
+		s = s.parent
+	}
+}
+
 func (ve *Versioned) set(r *Revision, val interface{}) {
 	s := r.current
 	_, ok := ve.versions.Load(s.version)
